9eca936f34a319164fe1ced475b0bd03055d8b85
Yabie-5 <e205744@ie.u-ryukyu.ac.jp>
2022-09-23 14:49:36 +0900

    Generate migrate file

---
+++ backend/app/db/migrations/versions/479bba4978f7_create_first_tables.py
@@ -0,0 +1,26 @@
+"""create_first_tables
+
+Revision ID: 479bba4978f7
+Revises: 
+Create Date: 2022-09-23 05:46:34.277673
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+
+# revision identifiers, used by Alembic
+revision = '479bba4978f7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
